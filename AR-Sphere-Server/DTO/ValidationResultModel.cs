ff8250c22282d59a7f6af15573ed32259fee5cff
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-07 22:42:01 -0500

    Format for a validation result.

---
+++ AR-Sphere-Server/DTO/ValidationResultModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSphere.DTO
+{
+	public class ValidationResultModel
+	{
+		public Dictionary<string, List<string>> Errors { get; set; }
+
+		public ValidationResultModel(Dictionary<string, List<string>> errors)
+		{
+			Errors = errors;
+		}
+	}
+}
