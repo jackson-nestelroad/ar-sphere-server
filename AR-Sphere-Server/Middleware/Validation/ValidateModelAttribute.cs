f0a24ba299d8e94e76e78906f765bdc78b83d253
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-07 22:42:17 -0500

    Filter for validating model state and returning errors.

---
+++ AR-Sphere-Server/Middleware/Validation/ValidateModelAttribute.cs
@@ -0,0 +1,28 @@
+﻿using ARSphere.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSphere.Middleware.Validation
+{
+	public class ValidateModelAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (!context.ModelState.IsValid)
+			{
+				var errorList = context.ModelState
+					.Where(kvp => kvp.Value.Errors.Count > 0)
+					.ToDictionary(
+						kvp => kvp.Key,
+						kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
+					);
+				context.HttpContext.Response.StatusCode = 422;
+				context.Result = new ObjectResult(new ResultViewModel(false, new ValidationResultModel(errorList)));
+			}
+		}
+	}
+}
