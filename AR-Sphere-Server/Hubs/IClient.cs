7bd43c1a1fb76defea4d87d7b2e25031464d3439
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-14 11:54:17 -0500

    Client interface to represent messages sent to client connections.

---
+++ AR-Sphere-Server/Hubs/IClient.cs
@@ -0,0 +1,14 @@
+﻿using ARSphere.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSphere.Hubs
+{
+	public interface IClient
+	{
+		Task Pong(string message);
+		Task AnchorData(IEnumerable<AnchorViewModel> anchors);
+	}
+}
