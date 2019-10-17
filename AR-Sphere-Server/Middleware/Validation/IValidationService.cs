33257385359afb32355a0a5339a5ab0502f0d7f6
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-17 10:02:50 -0500

    Transient validation service to validate models.

---
+++ AR-Sphere-Server/Middleware/Validation/IValidationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ARSphere.Middleware.Validation
+{
+	public interface IValidationService
+	{
+		public bool IsValid();
+
+		public void AddError(string prop, string message);
+		public bool AddError(string prop, string key, string message);
+		public bool RemoveError(string prop, string key);
+		public string GetError(string prop, string key);
+		public bool SetError(string prop, string key, string message);
+
+		public int GetErrorCount(string prop);
+
+		public string ToString();
+		public object ToObject();
+		public void Throw();
+
+		public string this[string prop, string key] { get; set; }
+
+		public void Validate(object obj);
+		public void Validate<T>(T obj, Func<T, IEnumerable<ValidationResult>> validator);
+
+		public bool TryValidate(object obj);
+		public bool TryValidate<T>(T obj, Func<T, IEnumerable<ValidationResult>> validator);
+	}
+}
