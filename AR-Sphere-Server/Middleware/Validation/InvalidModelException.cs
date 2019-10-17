b3de8d357681f83a4b53d3b9809af045b19475e0
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-17 10:04:43 -0500

    Custom exception for validation service to throw when model fails.

---
+++ AR-Sphere-Server/Middleware/Validation/InvalidModelException.cs
@@ -0,0 +1,19 @@
+﻿using ARSphere.Middleware.ExceptionHandling;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSphere.Middleware.Validation
+{
+	/// <summary>
+	/// <para>Exception to be thrown when model validation in the <code>ValidationService</code> fails.</para>
+	/// </summary>
+	public class InvalidModelException : HttpStatusCodeException
+	{
+		public InvalidModelException(ValidationService validationService) 
+			: base(StatusCodes.Status422UnprocessableEntity, validationService.ToObject())
+		{ }
+	}
+}
