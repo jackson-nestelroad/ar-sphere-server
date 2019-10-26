f03287ab53eeba492871c9faa45ab369aa870d2d
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-26 15:08:44 -0500

    Create dictionary of clients with relevant information for them.

--- AR-Sphere-Server/Hubs/MasterHub.cs
+++ AR-Sphere-Server/Hubs/MasterHub.cs
@@ -11,10 +11,36 @@ namespace ARSphere.Hubs
     public partial class MasterHub : Hub<IClient>
     {
         private IAnchorService _anchorService;
+        private Dictionary<string, Client> ClientMap;
+
+        private Client CurrentClient => ClientMap[Context.ConnectionId];
+
         public MasterHub(
             IAnchorService anchorService)
         {
             _anchorService = anchorService;
+            ClientMap = new Dictionary<string, Client>();
+        }
+
+        public string Ping(string message)
+        {
+            return message;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            ClientMap.Add(Context.ConnectionId, new Client
+            {
+                X = double.NaN,
+                Y = double.NaN
+            });
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientMap.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
