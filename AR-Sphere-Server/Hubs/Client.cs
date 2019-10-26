88ac75ae9e30a52191eafb29b07b8a15765f95ba
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-26 15:07:45 -0500

    Client class and interface to hold data and methods.

---
+++ AR-Sphere-Server/Hubs/Client.cs
@@ -0,0 +1,25 @@
+﻿using ARSphere.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSphere.Hubs
+{
+    /// <summary>
+    /// <para>List of methods to be called by SingalR on the client.</para>
+    /// </summary>
+    public interface IClient
+    {
+
+    }
+
+    /// <summary>
+    /// <para>Represents a single Client connection and associated identification data.</para>
+    /// </summary>
+    public class Client
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
