b543d668f10775594a3231f2f8d0fabe12bec7a6
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-10-27 22:05:24 -0500

    Seed Anchor entities and use System.Text.Json.

--- AR-Sphere-Server/Persistent/Helpers/DatabaseSeeder.cs
+++ AR-Sphere-Server/Persistent/Helpers/DatabaseSeeder.cs
@@ -1,5 +1,7 @@
 ﻿using ARSphere.Entities;
-using Newtonsoft.Json;
+using NetTopologySuite.Geometries;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -29,11 +31,31 @@ namespace ARSphere.Persistent.Helpers
             }
 
             var dataSet = File.ReadAllText(filePath);
-            var seedData = JsonConvert.DeserializeObject<List<User>>(dataSet);
+            var seedData = JsonSerializer.Deserialize<List<User>>(dataSet);
 
             foreach (var seedUser in seedData)
             {
                 _context.Users.Add(seedUser);
+                await _context.SaveChangesAsync();
+            }
+
+            return default;
+        }
+
+        public async Task<int> SeedAnchorEntitiesFromJson()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Persistent", "Helpers", "SeedData", "AnchorSeedData.json");
+            if(!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"The file {filePath} does not exist.");
+            }
+
+            var dataSet = File.ReadAllText(filePath);         
+            var seedData = JsonSerializer.Deserialize<List<Anchor>>(dataSet);
+
+            foreach(var seedAnchor in seedData)
+            {
+                _context.Anchors.Add(seedAnchor);
                 await _context.SaveChangesAsync();
             }
 
