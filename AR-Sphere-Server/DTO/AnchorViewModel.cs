c63d9c4f4beb496c9c8e57510a63c1e7188c3b3f
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-11-05 09:17:18 -0600

    Add AnchorLikedViewModel for like updates.

--- AR-Sphere-Server/DTO/AnchorViewModel.cs
+++ AR-Sphere-Server/DTO/AnchorViewModel.cs
@@ -16,6 +16,18 @@ namespace ARSphere.DTO
         public Point Location { get; set; }
         public UserViewModelPublic CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
+        // TODO: Change to LikeCount and LikedByUser
+        // LikedByUser would be retrieved by the current user's Authentication, since their ID is linked to it with each request
+        public List<int> LikedBy { get; set; }
+    }
+
+    /// <summary>
+    /// <para>Public data format to say a model's like field has been updated.</para>
+    /// </summary>
+    public class AnchorLikedViewModel : BaseViewModel
+    {
+        public string Id { get; set; }
+        public Point Location { get; set; }
         public List<int> LikedBy { get; set; }
     }
 }
